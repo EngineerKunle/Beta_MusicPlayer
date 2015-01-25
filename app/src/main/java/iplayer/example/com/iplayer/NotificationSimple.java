2ba76563d9f3383e97f1e4f29415e73ee01cff3c
Kunle Ogunjimi <kdotkid3@gmail.com>
2015-01-25 22:54:14 +0000

    NotificationSimple class completed

--- app/src/main/java/iplayer/example/com/iplayer/NotificationSimple.java
+++ app/src/main/java/iplayer/example/com/iplayer/NotificationSimple.java
@@ -1,5 +1,11 @@
 package iplayer.example.com.iplayer;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+
 /**
  * Created by EngineerKunle on 24/01/15.
  */
@@ -25,12 +31,55 @@ public class NotificationSimple {
         LAST_NOTIFICATION_ID++;
     }
 
+    /**
+     * Sends a quick text notification.
+     *
+     * @note This notification can be dismissed by the user and
+     *       if clicked won't do nothing.
+     *
+     * @param title Title of the notification.
+     * @param text  Text of the notification.
+     */
+    public void notify(Context c, String title, String text) {
+
+        Notification.Builder builder = new Notification.Builder(c);
+
+        builder.setSmallIcon(R.drawable.play)
+                .setContentTitle(title)
+                .setContentText(text);
+
+        NotificationManager manager = (NotificationManager)c.getSystemService(Context.NOTIFICATION_SERVICE);
+        manager.notify(NOTIFICATION_ID, builder.build()); //for api 11 you will need to use getNotification();
+    }
 
 
+    /**
+     * Sends a quick text redirectable notification.
+     *
+     * @note This notification can be dismissed by the user and
+     *       will be redirected to specified Activity if clicked.
+     *
+     * @param //where Class of the Activity it'll redirect when
+     *              it is clicked.
+     * @param title Title of the notification.
+     * @param text  Text of the notification.
+     */
+    public void notify(Context c, Class<?> toWhere, String title, String text) {
 
+        Notification.Builder builder = new Notification.Builder(c);
 
+        builder.setSmallIcon(R.drawable.play)
+                .setContentTitle(title)
+                .setContentText(text);
 
+        Intent intent = new Intent(c, toWhere);
+        PendingIntent pendingIntent = PendingIntent.getActivity(c, 0, intent, 0);
 
+        builder.setContentIntent(pendingIntent);
+
+        NotificationManager manager = (NotificationManager)c.getSystemService(Context.NOTIFICATION_SERVICE);
+        manager.notify(NOTIFICATION_ID, builder.build());
+    }
 
 
 }
