3fc7f36fb25b24cb22318dd0bdb1a7d79094ebc8
Kunle Ogunjimi <kdotkid3@gmail.com>
2015-01-25 21:02:09 +0000

    added the play.png

--- app/src/main/java/iplayer/example/com/iplayer/NotificationSimple.java
+++ app/src/main/java/iplayer/example/com/iplayer/NotificationSimple.java
@@ -4,4 +4,33 @@ package iplayer.example.com.iplayer;
  * Created by EngineerKunle on 24/01/15.
  */
 public class NotificationSimple {
+
+    /**
+     * Post a notification to be shown in the status bar. If a notification with thePlay.png
+     * same id has already been posted by your application and has not yet been canceled,
+     * it will be replaced by the updated information.
+     */
+
+    protected int NOTIFICATION_ID;
+
+    /**
+     * Counter to assure each created Notification gets
+     * an unique ID at runtime.
+     */
+    protected static int LAST_NOTIFICATION_ID = 1;
+
+
+    public NotificationSimple() {
+        NOTIFICATION_ID = LAST_NOTIFICATION_ID;
+        LAST_NOTIFICATION_ID++;
+    }
+
+
+
+
+
+
+
+
+
 }
