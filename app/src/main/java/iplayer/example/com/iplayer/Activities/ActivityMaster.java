b839b0e15081cfea6961512a1db58f3a9f428f47
Kunle Ogunjimi <kdotkid3@gmail.com>
2015-05-03 22:25:42 +0100

    building the now playing activity

--- app/src/main/java/iplayer/example/com/iplayer/Activities/ActivityMaster.java
+++ app/src/main/java/iplayer/example/com/iplayer/Activities/ActivityMaster.java
@@ -4,6 +4,11 @@ import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import iplayer.example.com.iplayer.IpMain;
+import iplayer.example.com.iplayer.R;
 
 /**
  * Created by EngineerKunle on 19/04/15.
@@ -14,12 +19,8 @@ public class ActivityMaster extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        return super.onCreateOptionsMenu(menu);
+        super.onCreate(savedInstanceState);
     }
 
     @Override
@@ -27,5 +28,29 @@ public class ActivityMaster extends Activity {
         // Every time the user focuses this Activity,
         // we need to check it.
         super.onResume();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_context, menu);
+
+        if(IpMain.mainMenuHasNowPlayingItem){
+            menu.findItem(R.id.context_menu_now_playing).setVisible(true);
+
+        }
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+
+
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
